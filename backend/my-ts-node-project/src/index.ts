29f33499e77c8fc75bfd781b1f720ea4232de62b
paulo <paulorodriguez733@gmail.com>
2025-06-13 14:57:51 -0500

    Envio del segundo commit

--- backend/my-ts-node-project/src/index.ts
+++ backend/my-ts-node-project/src/index.ts
@@ -1,23 +1,24 @@
-import express from 'express';
-import users from "./data/users";
-
+import express, { Request, Response } from 'express';
+import users from './data/users';
 
 const app = express();
 
-
 app.use(express.json());
 
-
-app.get('/', (req, res) => {
-  res.json('¡Bienvenidos!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("No hay usuarios");
 });
 
-app.get('/users', (req, res) => {
-    res.json(users);
-})
+app.get("/api", (req: Request, res: Response) => {
+  res.json("users para obtener los usuarios");
+});
 
+app.get("/api/users", (req: Request, res: Response) => {
+  res.json(users);
+});
 
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -1,11 +1,30 @@
+import { useState, useEffect } from "react";
 import UserCard from './components/UserCard';
 
+interface User {
+    id: number;
+    name: string;
+    age: number;
+}
 
 const App = () => {
+    const [users, setUsers] = useState<User[] | null>(null);
+
+
+    useEffect(() => {
+        fetch("/api/users")
+            .then((response) => response.json())
+            .then(data => setUsers(data))
+            .catch((err) => console.error(err));
+    }, []);
+    if (users === null) {
+        return <div>No Hay Usuarios</div>
+    }
     return(
         <div className="app">
-            <UserCard name="Antonio" age={4} />
-            <UserCard name="Antoniio 2" age={4} />
+            {users.map(users =>{
+                return <UserCard key={users.id} name={users.name} age={users.age} />
+            })}
 
         </div>
     );
